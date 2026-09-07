afd510db64b35fcd826ff8215e5d3d09f768198e
perediiinnn <benjamin.peredo.d@mail.pucv.cl>
2026-09-07 11:11:04 -0300

    test: se provoca fallo controlado de CI

--- frontend/src/app/app.spec.ts
+++ frontend/src/app/app.spec.ts
@@ -15,6 +15,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Catálogo de Recursos');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Título incorrecto');
   });
 });
